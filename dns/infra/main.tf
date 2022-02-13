dd64bcda8e943b929a1bcd05a80c6be8c7b66cf6
andrew.greene <andrew@defenseunicorns.com>
2022-02-13 15:36:04 -0700

    DNS rebuild

--- dns/infra/main.tf
+++ dns/infra/main.tf
@@ -11,18 +11,51 @@ provider "proxmox" {
   pm_tls_insecure = true
 }
 
+
 resource "proxmox_vm_qemu" "dns01" {
   name = "dns01"
   target_node = "bane"
+  ipconfig0 = "ip=192.168.1.198/24,gw=192.168.1.1"
   agent = var.qemu_agent
   clone = var.clone_template
   memory = var.memory
   cores = var.cores
+  os_type   = var.os_type
+  nameserver = var.dns_servers
+  sshkeys = var.ssh_key_public
+  ciuser = var.ssh_user
+  cipassword = var.user_secret
+
+  disk {
+    type = var.disk_type
+    storage = var.storage_pool
+    size = var.disk_size
+  }
 
   provisioner "file" {
-    source = "scripts/configure.sh"
-    destination = "/tmp/configure.sh"
+    source = "scripts/adguardsync.sh"
+    destination = "/tmp/adguardsync.sh"
 
+    connection {
+      type     = "ssh"
+      user     = var.ssh_user
+      private_key = "${file(var.ssh_key)}"
+      host = self.ssh_host
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v",
+      "curl -sSLO https://go.dev/dl/go1.17.7.linux-amd64.tar.gz",
+      "sudo tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz",
+      "export GOPATH=/home/user/go",
+      "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin",
+      "go install github.com/bakito/adguardhome-sync@latest",
+      "cp /tmp/adguardsync.sh /home/user/adguardsync.sh && chmod +x /home/user/adguardsync.sh",
+      "echo 'export GOPATH=/home/user/go' >> /home/user/.bashrc",
+      "echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> /home/user/.bashrc",
+    ]
     connection {
       type     = "ssh"
       user     = var.ssh_user
@@ -35,15 +68,47 @@ resource "proxmox_vm_qemu" "dns01" {
 resource "proxmox_vm_qemu" "dns02" {
   name = "dns02"
   target_node = "revan"
+  ipconfig0 = "ip=192.168.1.199/24,gw=192.168.1.1"
   agent = var.qemu_agent
   clone = var.clone_template
   memory = var.memory
   cores = var.cores
+  os_type   = var.os_type
+  nameserver = var.dns_servers
+  sshkeys = var.ssh_key_public
+  ciuser = var.ssh_user
+  cipassword = var.user_secret
+
+  disk {
+    type = var.disk_type
+    storage = var.storage_pool
+    size = var.disk_size
+  }
 
   provisioner "file" {
-    source = "scripts/configure.sh"
-    destination = "/tmp/configure.sh"
+    source = "scripts/adguardsync.sh"
+    destination = "/tmp/adguardsync.sh"
 
+    connection {
+      type     = "ssh"
+      user     = var.ssh_user
+      private_key = "${file(var.ssh_key)}"
+      host = self.ssh_host
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v",
+      "curl -sSLO https://go.dev/dl/go1.17.7.linux-amd64.tar.gz",
+      "sudo tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz",
+      "export GOPATH=/home/user/go",
+      "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin",
+      "go install github.com/bakito/adguardhome-sync@latest",
+      "cp /tmp/adguardsync.sh /home/user/adguardsync.sh && chmod +x /home/user/adguardsync.sh",
+      "echo 'export GOPATH=/home/user/go' >> /home/user/.bashrc",
+      "echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> /home/user/.bashrc",
+    ]
     connection {
       type     = "ssh"
       user     = var.ssh_user
--- dns/infra/variables.tf
+++ dns/infra/variables.tf
@@ -1,19 +1,19 @@
 variable "memory" {
   description = "Memory for each node"
   type        = string
-  default     = "4096"
+  default     = "8192"
 }
 
 variable "cores" {
   description = "Cores for each node"
   type        = number
-  default     = 4
+  default     = 2
 }
 
 variable "clone_template" {
   description = "Source template to clone"
   type        = string
-  default     = "ubuntu-template"
+  default     = "ci-ubuntu-template"
 }
 
 variable "qemu_agent" {
@@ -32,4 +32,44 @@ variable "ssh_key" {
   description = "SSH private key"
   type = string
   default = "~/.ssh/id_ed25519"
+}
+
+variable "ssh_key_public" {
+  description = "SSH public key"
+  type = string
+  default = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5gDqGux/p7JR/I/mBE/LYoJc8RBdSikmyVj7OTqBMW andrewsgreene89@gmail.com
+EOF
+}
+
+variable "os_type" {
+  description = "OS Type, cloud-init"
+  type = string
+  default = "cloud-init"
+}
+
+variable "dns_servers" {
+  description = "DNS server address"
+  type = string
+  default = "192.168.1.198 192.168.1.199"
+}
+
+variable "user_secret" {
+  description = "User secret"
+  type = string
+}
+
+variable "disk_type" {
+  type = string
+  default = "scsi"
+}
+
+variable "storage_pool" {
+  type = string
+  default = "local-lvm"
+}
+
+variable "disk_size" {
+  type = string
+  default = "75G"
 }
