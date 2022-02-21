1130b1e8955bf030e43eb4a949106e7a54f6d2ed
andrew.greene <andrew@defenseunicorns.com>
2022-02-21 16:08:51 -0700

    mgmt: flux bootstrap script update

--- mgmt/flux-bootstrap.sh
+++ mgmt/flux-bootstrap.sh
@@ -2,4 +2,4 @@
 
 [ -z $GITHUB_TOKEN ] && echo "no github token found"; exit
 
-flux bootstrap github --owner=andrewsgreene --repository=hac --path=mgmt/cluster --personal
+flux bootstrap github --owner=andrewsgreene --repository=hac --path=mgmt/cluster --personal=true
