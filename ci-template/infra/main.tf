cb111406e630fa6eec2f4d92c669e52d6a95c5a7
andrew.greene <andrew@defenseunicorns.com>
2022-02-26 07:57:24 -0700

    ci-template: update to v1

---
+++ ci-template/infra/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "ci-ubuntu-template-v1" {
+  name = "ci-ubuntu-template-v1"
+  target_node = "bane"
+  ipconfig0 = "ip=192.168.1.190/24,gw=192.168.1.1"
+  agent = var.qemu_agent
+  clone = var.clone_template
+  memory = var.memory
+  cores = var.cores
+  os_type   = var.os_type
+  nameserver = var.dns_servers
+  sshkeys = var.ssh_key_public
+  ciuser = var.ssh_user
+  cipassword = var.user_secret
+
+  disk {
+    type = var.disk_type
+    storage = var.storage_pool
+    size = var.disk_size
+  }
+}
