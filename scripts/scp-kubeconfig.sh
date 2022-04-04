668b7b62d2bc2ad0014fb0dcbe0cf25434f423c1
andrew.greene <andrew@defenseunicorns.com>
2022-04-03 22:29:27 -0600

    scripts: remote copy kubeconfig

--- scripts/scp-kubeconfig.sh
+++ scripts/scp-kubeconfig.sh
@@ -11,4 +11,4 @@ source_ip=192.168.1.$1
 dest_file=$HOME/.kube/$2
 
 scp user@$source_ip:/etc/rancher/k3s/k3s.yaml $dest_file
-sed -i'' -e "s|127.0.0.1|$source_ip|g" $dest_file
+sed -i'.bak' "s|127.0.0.1|$source_ip|g" $dest_file
