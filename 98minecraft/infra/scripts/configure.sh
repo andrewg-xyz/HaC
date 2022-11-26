0dbca9f0d6ac609e26e443809818fac0cb9d025e
andrew.greene <andrew@defenseunicorns.com>
2022-11-25 18:04:28 -0700

    feat: minecraft-ernetes

--- 98minecraft/infra/scripts/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install software-properties-common
-sudo apt update
-sudo apt install openjdk-17-jdk-headless screen -y
-
-sudo mkdir $HOME/minecraft
-sudo cp /tmp/server.jar $HOME/minecraft
--- cluster/h8s/flux/flux-bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-[ -z $GITHUB_TOKEN ] && echo "no github token found"; exit
-
-flux bootstrap github --owner=andrewsgreene --repository=hac --path=cluster/h8s/flux --personal=true
